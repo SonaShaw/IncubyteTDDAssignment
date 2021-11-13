b5e4b12429dbcd59a47f10d99a5ca6525db535e9
Sona Shaw <sonashawprofessional@gmail.com>
2021-11-13 13:32:10 +0530

    Tests for basic string done

--- src/com/incubyte/tdd/StringCalculator.java
+++ src/com/incubyte/tdd/StringCalculator.java
@@ -8,7 +8,13 @@ public class StringCalculator {
 	public int Add(String numbers) {
 		if(numbers.length()==0)
 			return 0;
-		return 1;
+		String numberArray[] = numbers.split(",");
+		int sum = 0;
+		for(String number : numberArray) {
+			int value = Integer.parseInt(number);
+			sum = sum + value;
+		}
+		return sum;
 	}
 	
 	
--- src/com/incubyte/tdd/StringCalculatorTest.java
+++ src/com/incubyte/tdd/StringCalculatorTest.java
@@ -4,15 +4,31 @@ import static org.junit.jupiter.api.Assertions.*;
 
 import org.junit.jupiter.api.Test;
 
-import com.incubyte.tdd.StringCalculator;
 
 class StringCalculatorTest {
 
 	@Test
-	void test1() {
+	void testForBasicString() {
 		StringCalculator stringCalculator = new StringCalculator();
 		String numbers = "";
 		int res = stringCalculator.Add(numbers);
+		assertEquals(res, 0);
+		
+		numbers = "1";
+		res = stringCalculator.Add(numbers);
+		assertEquals(res, 1);
+		
+		numbers = "1,2";	
+		res = stringCalculator.Add(numbers);
+		assertEquals(res, 3);
+		
+		//test if the string contains only "," 
+		numbers = ",";	
+		res = stringCalculator.Add(numbers);
+		assertEquals(res, 0);
+		
+		numbers = ",,,,,,";	
+		res = stringCalculator.Add(numbers);
 		assertEquals(res, 0);
 	}
 
