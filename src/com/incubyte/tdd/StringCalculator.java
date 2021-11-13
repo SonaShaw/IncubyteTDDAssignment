73579d460417efad397dbc4844979296ff13aae2
Sona Shaw <sonashawprofessional@gmail.com>
2021-11-13 13:15:54 +0530

    Basic Unit test passed

---
+++ src/com/incubyte/tdd/StringCalculator.java
@@ -0,0 +1,16 @@
+package com.incubyte.tdd;
+
+public class StringCalculator {
+	
+	
+	public StringCalculator() {}
+	
+	public int Add(String numbers) {
+		if(numbers.length()==0)
+			return 0;
+		return 1;
+	}
+	
+	
+
+}
--- src/com/incubyte/tdd/StringCalculatorTest.java
+++ src/com/incubyte/tdd/StringCalculatorTest.java
@@ -4,11 +4,16 @@ import static org.junit.jupiter.api.Assertions.*;
 
 import org.junit.jupiter.api.Test;
 
+import com.incubyte.tdd.StringCalculator;
+
 class StringCalculatorTest {
 
 	@Test
-	void test() {
-		fail("Not yet implemented");
+	void test1() {
+		StringCalculator stringCalculator = new StringCalculator();
+		String numbers = "";
+		int res = stringCalculator.Add(numbers);
+		assertEquals(res, 0);
 	}
 
 }
