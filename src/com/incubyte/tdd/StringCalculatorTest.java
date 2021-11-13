63412f5c09cafc041f8c465c3441e2a3c8047bc0
Sona Shaw <sonashawprofessional@gmail.com>
2021-11-13 13:39:49 +0530

    Test for unknown amount of number is passed

--- src/com/incubyte/tdd/StringCalculatorTest.java
+++ src/com/incubyte/tdd/StringCalculatorTest.java
@@ -5,8 +5,11 @@ import static org.junit.jupiter.api.Assertions.*;
 import org.junit.jupiter.api.Test;
 
 
+
+
 class StringCalculatorTest {
 
+	//===========Test fot point 1  (handle basic string)
 	@Test
 	void testForBasicString() {
 		StringCalculator stringCalculator = new StringCalculator();
@@ -30,6 +33,27 @@ class StringCalculatorTest {
 		numbers = ",,,,,,";	
 		res = stringCalculator.Add(numbers);
 		assertEquals(res, 0);
+	}
+	
+	// Test for point 2 (handle unknown amount of number)
+	@Test
+	void unownAmountofNumberstests() {
+		StringCalculator stringCalculator = new StringCalculator();
+		String numbers = "5000,50000";
+		int res = stringCalculator.Add(numbers);
+		assertEquals(res, 55000);
+		
+		numbers = "100000,100000";
+		res = stringCalculator.Add(numbers);
+		assertEquals(res, 200000);
+		
+		numbers = "678951,8963213";
+		res = stringCalculator.Add(numbers);
+		assertEquals(res, 9642164);
+		
+		numbers = "789001,8967213";
+		res = stringCalculator.Add(numbers);
+		assertEquals(res, 9756214);
 	}
 
 }
